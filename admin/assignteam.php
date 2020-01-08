ab0033643aed96133a82f94264923505f163f15a
btmullin <benjamin.t.mullin@gmail.com>
2020-01-08 13:31:11 -0600

    Completing team assignment page attempt 1

--- admin/assignteam.php
+++ admin/assignteam.php
@@ -1,8 +1,7 @@
 <?php
 include '../php/raceresultsutilities.php';
 // If this isn't a post, then we should display the form first
-if (!isset($_POST['confirm']) AND
-	!isset($_POST['submit']))
+if (!isset($_POST['submit']))
 {
 	// This isn't a the initial submit or confirm so we should display the form
 	// to select the racers
@@ -35,61 +34,23 @@ if (!isset($_POST['confirm']) AND
 	</form>
 <?php
 }
-else if (isset($_POST['submit']))
-{
-	// The user selected the racers to duplicate, pull up the changes being
-	// proposed and ask for confirmation before making the changes
-?>
-	<form action="duplicateracer.php" method="post">
-	<?php
-		$dup_id = $_POST["dup_id"];
-		$prim_id = $_POST["prim_id"];
-		echo "<input type=\"hidden\" name=\"dup_id\" value=\"$dup_id\">";
-		echo "<input type=\"hidden\" name=\"prim_id\" value=\"$prim_id\">";
-		$mysqli = OpenRaceResultsDatabase();
-		$result = $mysqli->query("SELECT * FROM Racer WHERE RacerID=$dup_id");
-		$row = $result->fetch_array();
-		$dup_name = $row["FirstName"]." ".$row["LastName"];
-		$result = $mysqli->query("SELECT * FROM Racer WHERE RacerID=$prim_id");
-		$row = $result->fetch_array();
-		$prim_name = $row["FirstName"]." ".$row["LastName"];
-		echo "Replacing $dup_name with $prim_name<br><br>";
-		echo "These results:<br>";
-		$result = $mysqli->query("SELECT * FROM Result WHERE RacerID=$dup_id");
-		ResultToTable($result);
-		echo "<br>";
-		echo "Will be added to these results:<br>";
-		$result = $mysqli->query("SELECT * FROM Result WHERE RacerID=$prim_id");
-		ResultToTable($result);
-		echo "<br>";
-	?>
-	<input type="submit" value="Are You Sure?" name="confirm">
-	</form>
-<?php
-}
 else
 {
-	// The user has confirmed the changes
-	$dup_id = $_POST["dup_id"];
-	$prim_id = $_POST["prim_id"];
+	// The user has assigned a team
+	$racer_id = $_POST["racer_id"];
+	$team_id = $_POST["team_id"];
 	echo "Duplicate ID: $dup_id<br>Primary ID: $prim_id<br><br>";
 	
-	$mysqli = OpenRaceResultsDatabase();
-	$result = $mysqli->query("SELECT Result.*, EventView.FullName FROM Result, EventView WHERE Result.RacerID=$dup_id AND Result.EventID=EventView.EventID");
-	echo "Updating results for the following events:<br>";
+	// Assign
+	$result = RaceResultsQuery("INSERT INTO Affiliation (RacerID, TeamID) VALUES ($racer_id, $team_id)");
+	
+	// Show Team
+	$result = RaceResultsQuery("SELECT Name FROM Team WHERE TeamID=$team_id");
+	$row = $result->fetch_array();
+	$team_name = $row["Name"];
+	echo "<b>Team: $team_name</b><br>";
+	
+	$result = RaceResultsQuery("SELECT FirstName, LastName FROM Racer, Affiliation WHERE Racer.RacerID=Affiliation.RacerID AND Affiliation.TeamID=$team_id");
 	ResultToTable($result);
-	
-	// Make the updates
-	$result = $mysqli->query("UPDATE Racer SET PrimaryRacerID=$prim_id WHERE RacerID=$dup_id");
-	$result = $mysqli->query("UPDATE Result SET RacerID=$prim_id WHERE RacerID=$dup_id");
-	
-	echo "<a href=\"../viewskier.php?rid=$prim_id\">See the updated results</a>";
-	
-	/*
-	
-	UPDATE Racer SET PrimaryRacerID=$prim_id WHERE RacerID=$dup_id;
-	UPDATE Result SET RacerID=$prim_id WHERE RacerID=$dup_id;
-	
-	*/
 }
 ?>
