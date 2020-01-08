291591d08ae641cf755090fa51cacaf086ee1203
btmullin <benjamin.t.mullin@gmail.com>
2020-01-08 14:34:19 -0600

    Team summary and view

--- admin/viewteam.php
+++ admin/viewteam.php
@@ -1,7 +1,7 @@
 <?php
 include '../php/raceresultsutilities.php';
 // If this isn't a post, then we should display the form first
-if (!isset($_POST['submit']))
+if (!isset($_POST['submit']) AND !isset($_REQUEST['rid']))
 {
 	// This isn't a the initial submit or confirm so we should display the form
 	// select the team
@@ -19,13 +19,50 @@ if (!isset($_POST['submit']))
 		?>
 	</select><br>
 	<input type="submit" value="View Team" name="submit">
-	</form>
+	</form><br>
+	
+	<?php
+	// Make a table of teams and number of members
+	echo "<table>";
+	//header
+	echo "<tr>";
+	$data = RaceResultsQuery("select TeamID, Name, count(Affiliation.TeamID) as \"Members\" FROM Team, Affiliation WHERE Team.TeamID=Affiliation.TeamID GROUP By Affiliation.TeamID");
+	$header = $data->fetch_fields();
+	foreach ($header as $col)
+	{
+		if ($col->name != "TeamID")
+		{
+			echo "<th><b>$col->name</b></th>";
+		}
+	}
+	echo "</tr>";
+	// body
+	$lastdate = null;
+	while ($row = $data->fetch_assoc())
+	{
+		echo "<tr";
+		// if it exists, grab the event and racer ids
+		$team_id = $row["TeamID"];
+		$team_name = $row["Name"];
+		$count = $row["Members"];
+		echo "<td><a href=\"viewteam.php?team_id=".$team_id.">$team_name</a></td>";
+		echo "<td>$count</td>";
+		echo "</tr>";
+	}
+	echo "</table>";
+	?>
 <?php
 }
 else
 {
-	// The user has assigned a team
-	$team_id = $_POST["team_id"];
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		// The user has assigned a team
+		$team_id = $_POST["team_id"];
+	}
+	else
+	{
+		$team_id = $_REQUEST["team_id"];
+	}
 	
 	// Show Team
 	$result = RaceResultsQuery("SELECT Name FROM Team WHERE TeamID=$team_id");
