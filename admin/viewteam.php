8060f6cc9b10babd5655c3101d59f3ddb714f734
btmullin <benjamin.t.mullin@gmail.com>
2020-01-08 14:50:58 -0600

    Applying css to tables

--- admin/viewteam.php
+++ admin/viewteam.php
@@ -1,3 +1,15 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Detailed view of the results of an individual skier.">
+	<meta name="author" content="Ben Mullin">
+    <title>Nordic Race Analysis Tools</title>
+    
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-" crossorigin="anonymous">
+	<link rel="stylesheet" href="../css/layouts/side-menu.css">
+	<link rel="stylesheet" href="../css/nratstyle.css">
+
+</head>
 <?php
 include '../php/raceresultsutilities.php';
 // If this isn't a post, then we should display the form first
