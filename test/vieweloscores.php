8fce40892a671ba39195ba35e5afe47f4bdf3b49
btmullin <benjamin.t.mullin@gmail.com>
2020-01-11 23:34:47 -0600

    Elo skier scores

--- test/vieweloscores.php
+++ test/vieweloscores.php
@@ -72,7 +72,7 @@ while ($row = $data->fetch_assoc())
 			echo "<td>";
 			if ($racer_id != null)
 			{
-				echo '<a href="vieweloskiersores.php?rid='.$racer_id.'">'.$field.'</a>';
+				echo '<a href="vieweloskierscores.php?rid='.$racer_id.'">'.$field.'</a>';
 			}
 			else
 			{
